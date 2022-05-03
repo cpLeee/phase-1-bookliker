74fdda89dd00537add281938591b0201db7299c5
Mai Vang <vmaineng@gmail.com>
2022-05-03 14:07:11 -0700

    list books and show details

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {});
+
+
+//grabbing data from localhost
+fetch('http://localhost:3000/books')
+    .then(function(response){
+         return response.json();
+    })
+    .then(function(data){
+        data.forEach(book => {
+            let title = document.createElement('li')
+            title.textContent = book.title
+            document.querySelector('#list').appendChild(title)
+
+       
+            //show Details
+            title.addEventListener('mouseover', function(){
+                //let list = document.querySelector('#list')
+                let image = document.createElement('img') 
+                    image.textContent = book[img_url]
+                let heading = document.createElement('h1')
+                    heading.textContent = book.title
+                let description = document.createElement('p')
+                    description.textContent = book.description
+                let likers = document.createElement('li')  
+                document.querySelector('#show-panel').appendChild(image,heading, description,likers)
+            })
+            
+        });;
+    });
+
+//Show details from div#show-panel
+
+
+
+
